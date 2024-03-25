2b02f9631bc783aff148c8216e369aacb9a249ba
zacktagnan <zacktagnan@gmail.com>
2024-03-25 18:15:44 +0100

    AdminPanel-Category-Index-estilos.y.aplicar.permiso.sobre.BTN.crear

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -62,12 +62,13 @@ const deleteCategory =  (id) => {
                             <h3 class="text-lg font-semibold text-gray-900">
                                 Listado
                             </h3>
-                            <p class="mt-2 text-sm text-gray-700">
+                            <p class="mt-1 text-sm text-gray-500">
                                 Categorías registradas. Actualmente, [{{ categoriesTotal }}].
                             </p>
                         </div>
 
-                        <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                        <div v-if="$page.props.user.permissions.includes('create_categories')"
+                            class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                             <Link :href="route('categories.create')" title="Crear Categoría"
                                 class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                             Crear +
